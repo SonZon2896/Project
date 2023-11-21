b511eb04d4cd0b305f2f9222985c06e71db99946
Mark <markshif1@gmail.com>
2023-11-21 17:59:58 +0300

    update_weapon

--- Weapons/weapons.h
+++ Weapons/weapons.h
@@ -1,7 +1,31 @@
-class tapok
+class Trigger;
+class Point;
+
+
+
+
+
+class Tapok
 {
+public:
     double damage;
+    double time;
+    Point coord;
+    Trigger trigger;
+
+
+    Tapok(Point&& coordinate, Trigger&& trigger_, double time_sleep, double damage_)
+    {
+        damage = damage_;
+        time = time_sleep; 
+        coord = coordinate;
+        trigger = trigger_;
+    }
+
+    trigger.cocroachers
+
+
+
     
-    bool is_here();
 
 };
