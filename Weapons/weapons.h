614dee1cc2593aab5f3b7bf25bdac127152c2927
Mark <markshif1@gmail.com>
2023-11-21 19:31:43 +0300

    full_weapon

--- Weapons/weapons.h
+++ Weapons/weapons.h
@@ -1,4 +1,4 @@
-#include <head.h>
+#include <heads.h>
 class Trigger;
 class Point;
 
@@ -6,35 +6,57 @@ class Point;
 
 
 
-class Tapok
+
+
+class Weapon
 {
-public:
-    double damage;
-    double time;
-    Point coord;
-    Trigger trigger;
-
-
-    Tapok(Point&& coordinate, Trigger&& trigger_, double time_sleep, double damage_)
+protected:
+    Weapon(const Point& coordinate, const Trigger& trigger_, double time_sleep, double damage_)
     {
         damage = damage_;
         time = time_sleep; 
         coord = coordinate;
         trigger = trigger_;
     }
+public:
+    double damage;
+    double time;
+    Point coord;
+    Trigger trigger;
 
-    std::vector<Cocroach*> cocr = trigger_.cocroaches;
+    Weapon() = delete;
+
+
+    std::vector<Cocroach*> cocr = trigger.cocroaches;
 
     void DamageForCocroaches()
     {
         for(auto taracan: cocr)
-            taracan.health -= damage;
+            taracan->health -= damage;
     }
-
-    
-
+};
 
 
-    
+class Tapok : protected Weapon
+{
+public:
+    Tapok() = delete;
+    Tapok(const Point& start_pos) : Weapon(start_pos, {{0.0, 0.0}, {1.0,1.0}}, 2.0, 25.0) {};
+
+};
+
+class Dihlofoz : protected Weapon
+{
+public:
+    Dihlofoz() = delete;
+    Dihlofoz(const Point& start_pos) : Weapon(start_pos, {{0.0, 0.0}, {1.0,1.0}}, 2.0, 50.0) {};
+
+};
+
+class Catch : protected Weapon
+{
+public:
+    Catch() = delete;
+    Catch(const Point& start_pos) : Weapon(start_pos, {{0.0, 0.0}, {1.0,1.0}}, 2.0, 100.0) {};
 
 };
