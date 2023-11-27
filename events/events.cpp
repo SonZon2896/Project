278f58849ba26540035bfb1658b2aee643362972
mark <markshif1@gmail.com>
2023-11-27 17:11:12 +0300

    evil_woman

--- events/events.cpp
+++ events/events.cpp
@@ -1,6 +1,23 @@
+#include "events.h"
 
-#include<iostream>
 
-int main(){
-    return 0;
+std::string Event::Evil_Woman(double& money){
+    int count = 0;
+    std::vector<Cockroach*> cock = Cockroach::GetAll();
+    for(int i =0; i< cock.size(); i++){
+        if(cock[i]->health>0)
+            count++;
+    }
+    if(count < 30){
+        std::string good = "Good boys, i will even not kill you))";
+        return good;
+    } else{
+        money -= 1000.;
+        if(money < 0.)
+            money = 0.;
+        std::string bad = "Bad boys, you should be ready to my attack";
+        return bad;
+    }
 }
+
+
--- events/events.h
+++ events/events.h
@@ -1,9 +1,14 @@
 #pragma once
 #include <string>
+#include "../Headers/heads.h"
 
 class Event{
+private:
+    static inline double money_speed = 0.0;
+    
 public:
-    static std::string Comenda();
+    static inline double money = 0.0;
+    static std::string Evil_Woman(double& money_speed);
     static std::string Stipuha();
     static std::string Remont();
 };
