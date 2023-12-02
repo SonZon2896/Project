af85e0f511daeddbeefdca43f65ef7ccdba66889
EchoPr <arkasha.pronin.2005@gmail.com>
2023-12-02 14:19:33 +0300

    test

--- Main/Logic.cpp
+++ Main/Logic.cpp
@@ -15,7 +15,7 @@ double Event::money{100.};
 double Event::money_speed{100.};
 const bool Mark_Lox = true; // 1 december
 const bool Artem_Lox = true; // 2
-std::vector<Road> roads{
+std::vector<Road> roads {
     {{700., 250.}, {700., 350.}, {250., 350.}, Fridge::pos},
     {{100., 630.}, {250., 630.}, {250., 350.}, Fridge::pos},
     {{1050., 525.}, {550., 525.}, {550., 350.}, {250., 350.}, Fridge::pos},
@@ -50,7 +50,7 @@ void MakeWave()
         throw std::runtime_error("Making wave, when it started");
 
     ++num_of_wave;
-    
+
 
     wave.num = 3;
     wave.health = 100.;
